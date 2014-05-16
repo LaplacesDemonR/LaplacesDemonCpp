67f6c62821174d55a0b8b69fc8b26a589caa7fc5
Statisticat <statisticat@gmail.com>
2014-05-16 16:49:23 -0400

    Added distributions, reverted partial

--- src/Math.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LaplacesDemonCpp_partial_H
-#define _LaplacesDemonCpp_partial_H
-
-#include <RcppArmadillo.h>
-
-RcppExport SEXP partial(SEXP MODEL, SEXP PARM, SEXP DATA, SEXP INTERVAL);
-
-#endif
